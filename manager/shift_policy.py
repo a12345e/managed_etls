417ccc12b13fdcd48537e2814fdefe2455f96e80
a12345e <a12345e@gmail.com>
2023-10-13 01:18:49 +0300

    main

--- manager/shift_policy.py
+++ manager/shift_policy.py
@@ -2,9 +2,14 @@ from typing import Sequence
 from etl.etl_operator import  ETLOperator
 from etl.sensor_operator import  SensorOperator
 from manager.realm import  Realm
+from datetime import datetime
+
+
 class ShiftPolicy:
     def __init__(self,
                  realm: Realm,
+                 shift_start: datetime,
+                 shift_end: datetime,
                  operators: Sequence[ETLOperator],
                  sensors: Sequence[SensorOperator]):
-        pass
+    pass

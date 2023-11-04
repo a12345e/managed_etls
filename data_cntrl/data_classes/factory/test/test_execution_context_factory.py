ce2b5f50c10830ebcd1e554d177adb3013e5c6dd
a12345e <a12345e@gmail.com>
2023-11-04 23:59:13 +0200

    going good

--- data_cntrl/data_classes/factory/test/test_execution_context_factory.py
+++ data_cntrl/data_classes/factory/test/test_execution_context_factory.py
@@ -1,21 +1,56 @@
 import unittest
-
+from datetime import datetime, timedelta
 import pytest
 
 from data_cntrl.data_classes.factory.execution_context_factory import create_execution_context
+from data_cntrl.data_classes.factory.component_id_factory import create_component_id_number_description
+
+start = datetime.now()
 
 
 class ExecutionContextCreateTest(unittest.TestCase):
+
     @staticmethod
     def test_execution_context_factory_positive():
-        f = create_execution_context(principal='alon',category='circuit_breaker', details=['timeout=5', 'reason= not enough resources'])
-        assert f.category == 'circuit_breaker'
-        assert f.details == ['timeout=5', 'reason= not enough resources']
+        e = create_execution_context(principal='alon', shift_start=start,
+                                     shift_end=start+timedelta(days=1),
+                                     infrastructure_task='action=1000',
+                                     mission=create_component_id_number_description("production_zorem",1.1,
+                                                                                    "the zorem stuff"),
+                                     application=create_component_id_number_description("default",1.1,
+                                                                                        "the default application"))
+        assert e.principal == 'alon'
+        assert e.shift_start == start
+        assert e.shift_end == start + timedelta(days=1)
+        assert e.infrastructure_task == 'action=1000'
+        assert e.mission.name == "production_zorem"
+        assert e.mission.version == 1.1
+        assert e.mission.description == "the zorem stuff"
+        assert e.application.name == 'default'
+        assert e.application.version == 1.1
+        assert e.application.description == "the default application"
 
     @staticmethod
     def test_short_string():
         with pytest.raises(ValueError):
-            create_execution_context(category='cddd', details=['ti', 'reason= not enough resources'])
+            create_execution_context(principal='n', shift_start=start,
+                                     shift_end=start + timedelta(days=1),
+                                     infrastructure_task='action=1000',
+                                     mission=create_component_id_number_description("production_zorem", 1.1,
+                                                                                    "the zorem stuff"),
+                                     application=create_component_id_number_description("default", 1.1,
+                                                                                        "the default application"))
+
+
+def test_missing_shift_end():
+    with pytest.raises(ValueError):
+        create_execution_context(principal='hey', shift_start=start,
+                                 shift_end=None,
+                                 infrastructure_task='action=1000',
+                                 mission=create_component_id_number_description("production_zorem", 1.1,
+                                                                                "the zorem stuff"),
+                                 application=create_component_id_number_description("default", 1.1,
+                                                                                    "the default application"))
 
 
 if __name__ == '__main__':
--- data_cntrl/data_classes/model/execution_context.py
+++ data_cntrl/data_classes/model/execution_context.py
@@ -1,13 +1,14 @@
 import pydantic
 from datetime import datetime
+from typing_extensions import Annotated
 from data_cntrl.data_classes.model.component_id import ComponentId
 
 
 class ExecutionContext(pydantic.BaseModel):
-    principal:  pydantic.Annotated[pydantic.StrictStr, pydantic.StringConstraints(min_length=3)]
+    principal:  Annotated[pydantic.StrictStr, pydantic.StringConstraints(min_length=3)]
     shift_start: datetime
     shift_end: datetime
-    infrastructure_task: pydantic.Annotated[pydantic.StrictStr, pydantic.StringConstraints(min_length=3)]
+    infrastructure_task: Annotated[pydantic.StrictStr, pydantic.StringConstraints(min_length=3)]
     mission: ComponentId
     application: ComponentId
 
